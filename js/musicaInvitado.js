73eec679d14cc1696fda262abb4ff797a300d9ff
José Vera <profe.jomanuelvr@gmail.com>
2024-01-26 00:50:26 -0300

    se agrega efecto al icono de musica

--- js/musicaInvitado.js
+++ js/musicaInvitado.js
@@ -1,9 +1,5 @@
 document.getElementById('playButton').addEventListener('click', function() {
-    // Abre la ventana modal
-    var myModal = new bootstrap.Modal(document.getElementById('modalInvitado'));
-    myModal.show();
-
-    // Resto del código para reproducir/pausar el audio
+   
     var audio = document.getElementById('audio');
     if (audio.paused) {
         audio.play();
